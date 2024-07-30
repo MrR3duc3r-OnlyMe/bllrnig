9d92064b1672da300d7e93fb6b3a0be7228f5eb4
neth@pogi.ako <neth@pogi.ako>
2024-07-30 21:28:35 +0800

    somewon

--- script/event/restoreFont.js
+++ script/event/restoreFont.js
@@ -4,25 +4,72 @@ module.exports = {
     version: "1.0",
     credits: "Neth"
   },
-  handleEvent: async({api, event}) => {
-    if (!event.body || event.type !== "message_reaction" || event.senderID !== api.getCurrentUserID()) return;
+  handleEvent: async ({ api, event }) => {
+    if (!event.body || event.type !== "message_reaction") return;
     const fontMapping = {
-  "a": "𝚊", "b": "𝚋", "c": "𝚌", "d": "𝚍", "e": "𝚎", "f": "𝚋", "g": "𝚐", "h": "𝚑", "i": "𝚒", "j": "𝚓", "k": "𝚔", "l": "𝚕", "m": "𝚖",
-  "n": "𝚗", "o": "𝚘", "p": "𝚙", "q": "𝚚", "r": "𝚛", "s": "𝚜", "t": "𝚝", "u": "𝚞", "v": "𝚟", "w": "𝚠", "x": "𝚡", "y": "𝚢", "z": "𝚣",
-  "A": "𝙰", "B": "𝙱", "C": "𝙲", "D": "𝙳", "E": "𝙴", "F": "𝙵", "G": "𝙶", "H": "𝙷", "I": "𝙸", "J": "𝙹", "K": "𝙺", "L": "𝙻", "M": "𝙼",
-  "N": "𝙽", "O": "𝙾", "P": "𝙿", "Q": "𝚀", "R": "𝚁", "S": "𝚂", "T": "𝚃", "U": "𝚄", "V": "𝚅", "W": "𝚆", "X": "𝚇", "Y": "𝚈", "Z": "𝚉"
-};
-function revertFont(text) {
-  const reverseFontMapping = Object.fromEntries(
-    Object.entries(fontMapping).map(([key, value]) => [value, key])
-  );
-  return text.split("").map(char => reverseFontMapping[char] || char).join("");
-}
-    if (event.reaction === "👍") {
-      await api.editMessage(revertFont(event.body), event.messageID, () => {
-        return;
-      });
+      "a": "𝚊",
+      "b": "𝚋",
+      "c": "𝚌",
+      "d": "𝚍",
+      "e": "𝚎",
+      "f": "𝚋",
+      "g": "𝚐",
+      "h": "𝚑",
+      "i": "𝚒",
+      "j": "𝚓",
+      "k": "𝚔",
+      "l": "𝚕",
+      "m": "𝚖",
+      "n": "𝚗",
+      "o": "𝚘",
+      "p": "𝚙",
+      "q": "𝚚",
+      "r": "𝚛",
+      "s": "𝚜",
+      "t": "𝚝",
+      "u": "𝚞",
+      "v": "𝚟",
+      "w": "𝚠",
+      "x": "𝚡",
+      "y": "𝚢",
+      "z": "𝚣",
+      "A": "𝙰",
+      "B": "𝙱",
+      "C": "𝙲",
+      "D": "𝙳",
+      "E": "𝙴",
+      "F": "𝙵",
+      "G": "𝙶",
+      "H": "𝙷",
+      "I": "𝙸",
+      "J": "𝙹",
+      "K": "𝙺",
+      "L": "𝙻",
+      "M": "𝙼",
+      "N": "𝙽",
+      "O": "𝙾",
+      "P": "𝙿",
+      "Q": "𝚀",
+      "R": "𝚁",
+      "S": "𝚂",
+      "T": "𝚃",
+      "U": "𝚄",
+      "V": "𝚅",
+      "W": "𝚆",
+      "X": "𝚇",
+      "Y": "𝚈",
+      "Z": "𝚉"
+    };
+
+    function revertFont(text) {
+      const reverseFontMapping = Object.fromEntries(Object.entries(fontMapping).map(([key, value]) => [value, key]));
+      return text.split("").map(char => reverseFontMapping[char] || char).join("");
     }
-    
+    if (event.reaction === "👍" && event.senderID === api.getCurrentUserID()) {
+      setTimeout(() => api.editMessage(revertFont(event.body), event.messageID, () => {
+      return;
+      }), 500);
+    }
+
   }
 }
