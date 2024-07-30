cf4b71cb205b3de103b5276bbca92950f18e6c24
neth@pogi.ako <neth@pogi.ako>
2024-07-30 14:59:49 +0800

    lotku

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ const SCRIPT_PATH = path.join(__dirname, SCRIPT_FILE);
 const GIT = "https://github.com/MrR3duc3r-OnlyMe/bllrnig.git";
 
 function ProjectBotify(){
-const main = spawn(`git pull ${GIT} && npm install && npm update && node ${SCRIPT_PATH}`, [""], {
-  cwd: __dirname,
-  stdio: "inherit",
-  shell: true
-});
-
-main.on("close", (exitCode) => {
-  if (exitCode === 0) {
-    console.log("Code 0!");
-  } else if (exitCode === 1) {
-    console.log("Restarting with code 1");
-    ProjectBotify();
-  } else {
-    console.error(`Code ${exitCode}!`);
+exec(`git pull ${GIT} && npm install && npm update && node ${SCRIPT_PATH}`,
+(async (error,stdout,stderr) => {
+  if (error){
+    console.error(error);
   }
-});
+  if (stdout){
+    console.log(stdout);
+  }
+  if (stderr){
+    console.log(stderr);
+  }
+}));
 }
 ProjectBotify();
