ba708c2c884b49a43c4e6240c5d4d8de9679e125
neth@pogi.ako <neth@pogi.ako>
2024-07-30 15:33:17 +0800

    erg

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 const {
-  exec
+  exec,spawn
 } = require("child_process");
 const path = require("path");
+const chalk = require("chalk");
 
 const SCRIPT_FILE = "botify.js";
 const SCRIPT_PATH = path.join(__dirname, SCRIPT_FILE);
 const GIT = "https://github.com/MrR3duc3r-OnlyMe/bllrnig.git";
 
 async function ProjectBotify(){
- console.log("Project Botify by Kenneth Aceberos");
- console.log("Starting...");
+ console.log(chalk.blue("Project Botify") + "by Kenneth Aceberos");
+ console.log(`===== ${chalk.green("PLEASE WAIT...")} =====`);
  const execute = (async(cmd) => {
  await exec(cmd,
  (async (error,stdout,stderr) => {
   if (error){
-    console.error(error);
+   console.error(chalk.red("ERROR") + " • " + error);
   }
   if (stdout){
-    console.log(stdout);
+   console.log(chalk.green("SUCCESS") + " • " + error);
   }
   if (stderr){
-    console.log(stderr);
+   console.error(chalk.red("ERROR") + " • " + stderr);
   }
+  console.log(`===== [${chalk.green("DONE")}] • ${cmd} =====`);
   return;
 }));
 });
 await execute(`git pull ${GIT}`);
 await execute("npm install");
-await execute(`node ${SCRIPT_PATH}`)
+console.log(`===== ${chalk.green("EXECUTE COMPLETE!")} =====`);
+const main = await spawn("node", [SCRIPT_PATH], {
+  cwd: __dirname,
+  stdio: "inherit",
+  shell: true
+});
+
+main.on("close", (exitCode) => {
+  if (exitCode === 0) {
+    console.error(`${chalk.red("ERROR")} • Code ${exitCode}`);
+  } else if (exitCode === 1) {
+    console.error(`${chalk.red("ERROR")} • Code ${exitCode}`);
+    console.log(`===== ${chalk.green("RESTARTING...")} =====`);
+    ProjectBotify();
+  } else {
+    console.error(`${chalk.red("ERROR")} • Code ${exitCode}`);
+  }
+})
 }
 ProjectBotify();
