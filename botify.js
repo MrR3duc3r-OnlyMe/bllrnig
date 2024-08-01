c787f3cfaf2ce9cfbf125dff5234af40166c4ba0
neth@pogi.ako <neth@pogi.ako>
2024-08-01 22:16:32 +0800

    cheg

--- botify.js
+++ botify.js
@@ -158,13 +158,11 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(async (request, response, next) => {
   response.set({
-    "Are-You-Kupal?": "NOPE",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers": "Are-You-Kupal?, Content-Type, X-Requested-With, Accept",
+    "Access-Control-Allow-Headers": "Content-Type, X-Requested-With, Accept",
   });
   
-  const t = request.headers.get("Are-You-Kupal?");
   if (request.method === 'OPTIONS') {
     response.sendStatus(200);
   } else {
